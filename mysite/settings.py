4827b3fe6be7bdbaa2428dab1999e9869c61429d
sa2ki-15 <yamaguchikazuki733@gmail.com>
2020-05-07 00:46:10 +0900

    Djangoチュートリアルその６まで

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# ここにはプロジェクト単位で使用する静的ファイルを配置する。
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
